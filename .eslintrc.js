ea0edcb50ad50f58ff1425181db5e881e6fadea4
Mikhail Nikolaev <nikolaevmikhail@gmail.com>
2021-06-19 14:28:42 +0300

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,20 @@
-// module.exports = {
-//   parser: "babel-eslint",
-//   extends: ["react-app", "prettier"],
-//   plugins: ["prettier"],
-//   rules: {
-//     "prettier/prettier": "error",
-//     quotes: ["error", "double", { allowTemplateLiterals: true }],
-//     endOfLine: "auto",
-//   },
-// };
-
 module.exports = {
   parser: `@typescript-eslint/parser`,
   extends: [
+    "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
+    "prettier",
   ],
   plugins: ["@typescript-eslint", "prettier"],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    project: "./tsconfig.json",
     sourceType: "module", // Allows for the use of imports
   },
   env: {
     browser: true,
     node: true,
+    es2021: true,
   },
   rules: {
     quotes: "off",
@@ -42,6 +33,24 @@ module.exports = {
         semi: false,
         singleQuote: false,
         printWidth: 120,
+      },
+    ],
+    "prettier/prettier": "error",
+    // this assumes that something that exports one item now will always do so
+    "import/prefer-default-export": "off",
+    // airbnb considers any loop restricted syntax O_o
+    "no-restricted-syntax": "off",
+    "no-continue": "off",
+    "no-await-in-loop": "off",
+    // Until this is resolved:
+    // https://github.com/typescript-eslint/typescript-eslint/issues/2077
+    "@typescript-eslint/camelcase": "off",
+    // "no-console": "off",
+    "no-underscore-dangle": ["error", { allow: ["_id"] }],
+    "no-restricted-imports": [
+      "error",
+      {
+        paths: ["lodash"],
       },
     ],
   },
