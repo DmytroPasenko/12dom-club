edf096f85829b337532e1546a1e4225985d95001
DmytroPasenko <elengardway@gmail.com>
2021-07-14 15:03:41 +0300

    TopNewsOptimized

--- src/components/Home/home-components/TopGridNews/TopGridNews.tsx
+++ src/components/Home/home-components/TopGridNews/TopGridNews.tsx
@@ -2,77 +2,62 @@ import { FunctionComponent } from "react";
 import { useTranslation } from "react-i18next";
 import classNames from "classnames";
 
-import { 
+import {
   post,
   postContent,
   postMeta,
   entryP,
   entryButton,
   entryIcon,
-  postImg
-} from "./TopGridNews.module.scss"
+  postImg,
+} from "./TopGridNews.module.scss";
 
-import firstSldieBg from "../../../../../static/img/home/slider/slide-1.jpg"
-import secondSldieBg from "../../../../../static/img/home/slider/slide-2.jpg"
-import thirdldieBg from "../../../../../static/img/home/slider/slide-3.jpg"
+interface newsRecord {
+  header: string;
+  date: number;
+  author: string;
+  text: string;
+  image: string;
+}
 
 const TopGridNews: FunctionComponent = () => {
   const { t } = useTranslation(["site", "top-grid-news"]);
 
+  const getNews = (): newsRecord[] => [
+    {
+      header: "Use your Jupiter luck in 2017",
+      date: new Date().getTime(),
+      author: "admin",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero eum nisi illo ullam neque est mollitia placeat, quis magni modi itaque distinctio perspiciatis quidem odit qui eveniet exaccusamus!",
+      image: "../../../../../img/home/top-grid-news/top-post-bg-1.jpg",
+    },
+    {
+      header: "Your aquarios factors and your...",
+      date: new Date().getTime(),
+      author: "admin",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque vero eum nisi illo ullam neque est mollitia placeat, quis magni modi itaque distinctio perspiciatis quidem odit qui eveniet exaccusamus!",
+      image: "../../../../../img/home/top-grid-news/top-post-bg-2.jpg",
+    },
+  ];
+
+  const news = getNews();
+
   return (
     <>
-      <div className={post}>
-          <div className={postContent}>
-            <div className={postMeta}>
-              <h5 className="uppercase">
-                <a href="#">Use your Jupiter luck in 2017</a>
-              </h5>
-              <p>
-                <a href="#">Nov. 8, 2016</a>
-                <span>- by</span>
-                <a href="#">admin</a>
-              </p>
-              <p className={entryP}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                vero eum nisi illo ullam neque est mollitia placeat, quis
-                magni modi itaque distinctio perspiciatis quidem odit qui
-                eveniet ex accusamus!
-              </p>
-              <button className={classNames(entryButton,"flex uppercase")}>
-                Read More
-                <img
-                  src="../../../../../svg/other/arrow.svg"
-                  alt="arrow"
-                  className={entryIcon}
-                />
-              </button>
-            </div>
-            <a href="#"
-              ><img
-                className={postImg}
-                src="../../../../../img/home/top-grid-news/top-post-bg-1.jpg"
-                alt="post-img"
-            /></a>
-          </div>
-        </div>
+      {news.map((item) => (
         <div className={post}>
           <div className={postContent}>
             <div className={postMeta}>
               <h5 className="uppercase">
-                <a href="#">Your aquarios factors and your...</a>
+                <a href="#">{item.header}</a>
               </h5>
               <p>
-                <a href="#">Oct. 26, 2016</a>
+                <a href="#">{new Date(item.date).toString()}</a>
                 <span>- by</span>
-                <a href="#">admin</a>
+                <a href="#">{item.author}</a>
               </p>
-              <p className={entryP}>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Eligendi voluptate excepturi eius obcaecati veniam! Unde sunt
-                cupiditate, aperiam reiciendis quam dolor perspiciatis id
-                perferendis reprehenderit sint quasi rerum voluptate quaerat.
-              </p>
-              <button className={classNames(entryButton,"flex uppercase")}>
+              <p className={entryP}>{item.text}</p>
+              <button className={classNames(entryButton, "flex uppercase")}>
                 Read More
                 <img
                   src="../../../../../svg/other/arrow.svg"
@@ -81,14 +66,12 @@ const TopGridNews: FunctionComponent = () => {
                 />
               </button>
             </div>
-            <a href="#"
-              ><img
-                className={postImg}
-                src="../../../../../img/home/top-grid-news/top-post-bg-2.jpg"
-                alt="post-img"
-            /></a>
+            <a href="#">
+              <img className={postImg} src={item.image} alt="post-img" />
+            </a>
           </div>
         </div>
+      ))}
     </>
   );
 };
