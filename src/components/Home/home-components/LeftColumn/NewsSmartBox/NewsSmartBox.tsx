54ddce484e662ff7890e40b352c6c3f3b5e098b3
DmytroPasenko <elengardway@gmail.com>
2021-07-20 10:53:02 +0300

    Trying to fix the conflict

--- src/components/Home/home-components/LeftColumn/NewsSmartBox/NewsSmartBox.tsx
+++ src/components/Home/home-components/LeftColumn/NewsSmartBox/NewsSmartBox.tsx
@@ -37,55 +37,7 @@ const NewsSmartBox: FunctionComponent = () => {
   const { locale } = useLocalization();
   const { posts } = useContext(IndexPageContext);
 
-  return (
-    <>
-      {mainNewsContent.map((item) => (
-        <div
-          className={classNames(newsBoxContainer, "lg:col-span-1 col-span-2")}
-        >
-          <div className={mainNews}>
-            <a href="http://localhost:8000/blogPost">
-              <div className={newsTextContent}>
-                <h5>{item.title}</h5>
-                <div className="normal-case flex">
-                  <p>{formatDate(item.date, locale)}</p>
-                  <span>- by</span>
-                  <p>{item.author}</p>
-                  <p>{item.comments}</p>
-                </div>
-              </div>
-              <img className={newsImg} src={item.image} alt="news-bg" />
-            </a>
-          </div>
-        </div>
-      ))}
-
-      {softNewsContent.map((item, idx) => (
-        <div
-          className={classNames(
-            newsBoxContainer,
-            { leftBox: isEven(idx) },
-            { rightBox: !isEven(idx) },
-            "sm:col-span-1 col-span-2",
-          )}
-        >
-          <div className={classNames(softNews, "text-black flex")}>
-            <a href="http://localhost:8000/blogPost" className="flex flex-row">
-              <img className={softNewsImg} src={item.image} alt="news-bg" />
-              <div className={classNames(newsTextContent, "flex flex-col")}>
-                <h5>{item.title}</h5>
-                <div className="normal-case flex items-center">
-                  <p className={aDate}>{formatDate(item.date, locale)}</p>
-                  <span>- by</span>
-                  <p>{item.author}</p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-      ))}
-    </>
-  );
+  return <></>;
 };
 
 export default NewsSmartBox;
