a42677e90d970f0d16982b81778f7fe72d43c249
DmytroPasenko <elengardway@gmail.com>
2021-07-20 11:45:46 +0300

    data for CustomPosts is fetched from CMS

--- src/components/Home/home-components/SideBar/CustomPosts/CustomPosts.tsx
+++ src/components/Home/home-components/SideBar/CustomPosts/CustomPosts.tsx
@@ -1,7 +1,6 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useContext } from "react";
 import { useTranslation } from "react-i18next";
 import classNames from "classnames";
-
 import { formatDate } from "@miran-soft/common";
 import { useLocalization } from "gatsby-theme-i18n";
 import {
@@ -14,44 +13,15 @@ import {
   leftBox,
   rightBox,
 } from "./CustomPosts.module.scss";
-
-interface CustomRecord {
-  class: string;
-  image: string;
-  date: number;
-  author: string;
-  title: string;
-}
+import { isEven } from "../../../../../utils/isEven";
+import IndexPageContext from "../../../../Context/IndexContext";
+import { getPropertyAsStr } from "../../../../../utils/getPropertyAsStr";
+import AppLink from "../../../../AppLink";
 
 const CustomPosts: FunctionComponent = () => {
   const { t } = useTranslation(["site", "custom-posts"]);
   const { locale } = useLocalization();
-
-  const getPosts = (): CustomRecord[] => [
-    {
-      class: classNames(customPost, leftBox, "text-black"),
-      image: "/img/home/side-bar/custom-posts/custom-post-1.jpg",
-      date: new Date().getTime(),
-      author: "admin",
-      title: "You have a power",
-    },
-    {
-      class: classNames(customPost, rightBox, "text-black"),
-      image: "/img/home/side-bar/custom-posts/custom-post-2.jpg",
-      date: new Date().getTime(),
-      author: "admin",
-      title: "We are energy",
-    },
-    {
-      class: classNames(customPost, leftBox, "text-black"),
-      image: "/img/home/side-bar/custom-posts/custom-post-3.jpg",
-      date: new Date().getTime(),
-      author: "admin",
-      title: "Forks int the road of life",
-    },
-  ];
-
-  const posts = getPosts();
+  const { posts } = useContext(IndexPageContext);
 
   return (
     <>
@@ -62,28 +32,48 @@ const CustomPosts: FunctionComponent = () => {
           "grid lg:grid-cols-1 sm:grid-cols-2 grid-cols-1 flex lg:flex-col flex-row",
         )}
       >
-        {posts.map((item) => (
-          <div className={item.class}>
-            <div className={classNames(customPostContent, "flex")}>
-              <a
-                href="http://localhost:8000/blogPost"
-                className="flex flex-row"
-              >
-                <img src={item.image} alt="news-bg" />
-                <div
-                  className={classNames(customPostTextContent, "flex flex-col")}
-                >
-                  <div className={classNames(customPostDate, "flex flex-row")}>
-                    <p className={aDate}>{formatDate(item.date, locale)}</p>
-                    <span>- by</span>
-                    <p>{item.author}</p>
+        {posts?.slice(6, 9).map((item, index) => {
+          const title = getPropertyAsStr(item, "title");
+          const image = getPropertyAsStr(item, "image");
+          const date = getPropertyAsStr(item, "date");
+          const formattedDate = date
+            ? formatDate(parseInt(date, 10), locale)
+            : "";
+          const author = getPropertyAsStr(item, "author");
+
+          return (
+            <div
+              className={classNames(
+                customPost,
+                { leftBox: isEven(index) },
+                { rightBox: !isEven(index) },
+                "flex flex-row",
+              )}
+              key={item.slug}
+            >
+              <div className={classNames(customPostContent, "flex flex-row")}>
+                <AppLink to={`news/${item.slug}`}>
+                  <img src={image} alt="news-bg" />
+                  <div
+                    className={classNames(
+                      customPostTextContent,
+                      "flex flex-col",
+                    )}
+                  >
+                    <div
+                      className={classNames(customPostDate, "flex flex-row")}
+                    >
+                      <p className={aDate}>{formattedDate}</p>
+                      <span>- by</span>
+                      <p>{author}</p>
+                    </div>
+                    <h5>{title}</h5>
                   </div>
-                  <h5>{item.title}</h5>
-                </div>
-              </a>
+                </AppLink>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
   );
