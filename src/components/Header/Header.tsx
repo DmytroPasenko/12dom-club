78a4428d03cd253e11fde6e2ba6d5fa7c7e56431
DmytroPasenko <elengardway@gmail.com>
2021-07-23 12:15:02 +0300

    Unnecessary elements have been removed from the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,27 +13,27 @@ import {
   moreThenBlack,
   subMenu,
   siteSubLink,
-  subMoreThenBlack,
+  // subMoreThenBlack,
   // activeLink,
-  subMoreThenContainer,
-  subSubMenu,
-  blogMiddleLi,
-  blogBottomLi,
-  middleLi,
+  // subMoreThenContainer,
+  // subSubMenu,
+  // blogMiddleLi,
+  // blogBottomLi,
+  // middleLi,
   bottomLi,
-  siteSubSubLink,
+  // siteSubSubLink,
   // activeNavigation,
   // currentPage,
   moreThenContainer,
   moreThen,
-  subMoreThen,
+  // subMoreThen,
   // currentSubLink,
   topLi,
   // currentSiteSubSubLink,
   linkList,
-  homeTopLi,
-  homeBottomLi,
-  blogTopLi,
+  // homeTopLi,
+  // homeBottomLi,
+  // blogTopLi,
   localizationImage,
   localizationMenu,
 } from "./Header.module.scss";
@@ -92,159 +92,19 @@ const Header: FunctionComponent<HeaderProps> = ({ title }) => {
                   href="http://localhost:8000/"
                 >
                   Home
-                  <div className={classNames(moreThenContainer, "flex")}>
-                    <img
-                      className={classNames(moreThen, "md:block hidden")}
-                      src={moreIcon}
-                      alt="more"
-                    />
-                    <img
-                      className={classNames(moreThenBlack, "md:hidden")}
-                      src={moreIcon}
-                      alt="more"
-                    />
-                  </div>
                 </a>
-                <ul
-                  className={classNames(
-                    subMenu,
-                    "flex flex-col md:normal-case",
-                  )}
-                >
-                  <li className={classNames(topLi, homeTopLi, "md:bg-white")}>
-                    <a
-                      href="http://localhost:8000/"
-                      className={classNames(siteSubLink, "flex")}
-                    >
-                      Header settings
-                      <div className={subMoreThenContainer}>
-                        <img
-                          className={classNames(subMoreThen)}
-                          src={moreIcon}
-                          alt="more"
-                        />
-                      </div>
-                    </a>
-                    <ul className={subSubMenu}>
-                      <li className={classNames(topLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 1
-                        </a>
-                      </li>
-                      <li className={classNames(middleLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 2
-                        </a>
-                      </li>
-                      <li className={classNames(middleLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 3
-                        </a>
-                      </li>
-                      <li className={classNames(middleLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 4
-                        </a>
-                      </li>
-                      <li className={classNames(middleLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 5
-                        </a>
-                      </li>
-                      <li className={classNames(middleLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 6
-                        </a>
-                      </li>
-                      <li className={classNames(bottomLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Header Style 7
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-
-                  <li
-                    className={classNames(
-                      bottomLi,
-                      homeBottomLi,
-                      "md:bg-white",
-                    )}
-                  >
-                    <a
-                      href="http://localhost:8000/"
-                      className={classNames(siteSubLink, "flex")}
-                    >
-                      Footer settings
-                      <div className={subMoreThenContainer}>
-                        <img
-                          className={classNames(subMoreThen, subMoreThenBlack)}
-                          src={moreIcon}
-                          alt="more"
-                        />
-                      </div>
-                    </a>
-                    <ul className={subSubMenu}>
-                      <li className={classNames(topLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Footer Style 1
-                        </a>
-                      </li>
-                      <li className={classNames(middleLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Footer Style 2
-                        </a>
-                      </li>
-                      <li className={classNames(bottomLi, "md:bg-white")}>
-                        <a
-                          href="http://localhost:8000/"
-                          className={siteSubSubLink}
-                        >
-                          Footer Style 3
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
               </li>
 
               <li>
                 <a
                   className={classNames(siteLink, "uppercase")}
-                  href="http://localhost:8000/"
+                  href="http://localhost:8000/about"
                 >
                   About me
                 </a>
               </li>
 
-              <li>
+              {/* <li>
                 <a
                   className={classNames(siteLink, linkList, "flex uppercase")}
                   href="http://localhost:8000/"
@@ -442,9 +302,9 @@ const Header: FunctionComponent<HeaderProps> = ({ title }) => {
                     </ul>
                   </li>
                 </ul>
-              </li>
+              </li> */}
 
-              <li>
+              {/* <li>
                 <a
                   className={classNames(siteLink, linkList, "flex uppercase")}
                   href="http://localhost:8000/"
@@ -489,12 +349,12 @@ const Header: FunctionComponent<HeaderProps> = ({ title }) => {
                     </a>
                   </li>
                 </ul>
-              </li>
+              </li> */}
 
               <li>
                 <a
                   className={classNames(siteLink, "uppercase")}
-                  href="http://localhost:8000/"
+                  href="http://localhost:8000/contacts"
                 >
                   Contacts
                 </a>
